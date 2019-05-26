6cd1d1c75010153e1082614917d3dfda5e33eed6
SergeyLykhachov <46016742+SergeyLykhachov@users.noreply.github.com>
2019-05-26 18:25:44 -0400

    Update Animator.java

--- src/com/yahoo/slykhachov/animator/Animator.java
+++ src/com/yahoo/slykhachov/animator/Animator.java
@@ -3,14 +3,10 @@ package com.yahoo.slykhachov.animator;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
-//import com.yahoo.slykhachov.animator.util.*;
-//import com.yahoo.slykhachov.animator.model.AnimationElement;
-//import com.yahoo.slykhachov.animator.model.TransformGroup;
 import com.yahoo.slykhachov.animator.model.AnimatorModel;
 import com.yahoo.slykhachov.animator.view.AnimatorView;
 
 public class Animator {
-	//public static final int FRAME_RATE = 40; // 25 frames/second
 	private Animation animation;
 	private AnimatorView animatorView;
 	private AnimatorModel animatorModel;
@@ -20,7 +16,7 @@ public class Animator {
 			d = new Dimension(800, 600);
 			System.out.println("Small Screen found");
 		} else {
-			//d = new Dimension(1024, 768);
+			d = new Dimension(1024, 768);
 			//d = new Dimension(1200, 800);
 			//d = new Dimension(1200, 750);
 		}
